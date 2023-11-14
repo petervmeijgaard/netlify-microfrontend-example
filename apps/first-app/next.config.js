1e0a85fef7a6323d359a5edf09acce3d6e760c12
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-11-14 22:26:48 +0100

    Try to setup CORS correctly

--- apps/first-app/next.config.js
+++ apps/first-app/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	assetPrefix: process.env.URL ?? undefined,
+	headers: () => [
+		{
+			source: "/_next/:path*",
+			headers: [
+				{
+					key: "Access-Control-Allow-Origin",
+					value: "https://petjinski-app-shell.netlify.app",
+				},
+			],
+		},
+	],
 };
 
 module.exports = nextConfig;
